66cf298a88f1185ac38a9abe150bdc21e9a6c349
Salomon Metre <kulosmetros088@gmail.com>
2023-07-14 22:17:14 +0300

    Add practice on the use of structs with different ways of passing parameters

--- Structs_and_Pointers/main.cpp
+++ Structs_and_Pointers/main.cpp
@@ -6,11 +6,32 @@ struct Student{
     int age{};
 };
 
-int main() {
-//    The C style
+struct Rectangle{
+    int breath;
+    int length;
+};
 
+void changeLengthAndShowPerimeter_Pointer(Rectangle *rectangle){
+    rectangle->length += 1;
+    cout<<"Perimeter is : "<<2*(rectangle->length+rectangle->breath)<<endl;
+}
+
+void changeLengthAndShowPerimeter_Reference(Rectangle &rectangle){
+    rectangle.length += 1;
+    cout<<"Perimeter is : "<<2*(rectangle.length+rectangle.breath)<<endl;
+}
+
+void changeLengthAndShowPerimeter_Value(Rectangle rectangle){
+    rectangle.length += 1;
+    cout<<"Perimeter is : "<<2*(rectangle.length+rectangle.breath)<<endl;
+}
+
+
+int main() {
+
+//    The C style
     struct Student* newStudent;
-    newStudent = (Student *) malloc(sizeof(Student));
+    newStudent = (Student*) malloc(sizeof(Student));
     newStudent->firstName = "Salomon";
     newStudent->age =21;
     cout<<newStudent->firstName<<endl;
@@ -22,5 +43,18 @@ int main() {
     anotherPointer->age =23;
     cout<<anotherPointer->firstName<<endl;
     cout<<anotherPointer->age<<endl;
+
+//    change the width of a rectangle
+    auto rectangle = Rectangle{12, 13};
+//    case 1
+    changeLengthAndShowPerimeter_Pointer(&rectangle);
+
+//    case 2
+    changeLengthAndShowPerimeter_Reference(rectangle);
+
+////    case 3
+    changeLengthAndShowPerimeter_Value(rectangle);
+
+    cout<<"Current length : "<<rectangle.length<<endl;
     return 0;
 }
